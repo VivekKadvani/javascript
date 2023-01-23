4d35990f6344556e82336512cf87656827b6f4f8
Vivek Kadvani <vivek.k@simformsolutions.com>
2023-01-23 13:52:03 +0530

    till const keyword

--- test.js
+++ test.js
@@ -1,7 +1,32 @@
-var v="hello world by vivek";
+//start file and test node 
+var v = "hello world by vivek";
 console.log(v);
 
-let b=65
+
+//start learning variable   
+let b = 65
 console.log(b)
-b="student"
+b = "student"
 console.log(b)
+
+//overriting var
+var a = 67
+console.log(a)
+var a = "sixty seven"
+console.log(a)
+let c = 12
+console.log(c)
+//let c="twelve"
+c = "twelve"
+console.log(c)
+{
+    let c = "bar"
+    console.log(c)
+}
+console.log(c)
+
+//const keyword : not changed or not redeclare
+const boss = "vivek";
+//boss="mw"
+//let boss="kak"
+console.log(boss)
