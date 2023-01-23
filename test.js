2bfacdcbb079b8447d87f8a6d024bac600d1320f
Vivek Kadvani <vivek.k@simformsolutions.com>
2023-01-23 14:58:42 +0530

    completed practice set 1

--- test.js
+++ test.js
@@ -30,3 +30,43 @@ const boss = "vivek";
 //boss="mw"
 //let boss="kak"
 console.log(boss)
+
+
+//datatypes
+let d=88
+let e=null
+let f=Symbol("i am vk")
+let g="this is js"
+let h=true
+let i=BigInt(345)
+let j
+console.log(d,e,f,g,h,i,j)
+
+const obj = {
+    "vivek":"head",
+    "role":"blockchain",
+    "package":6.5,
+    "training":null,
+    "onboarding":undefined
+}
+
+console.log(obj["package"])
+
+
+//practice set 1
+let k="vivek"
+k=k+23
+console.log(k)
+
+console.log(typeof(k))
+
+obj["time"]=10
+obj["vivek"]="student"
+console.log(obj)
+
+const dict ={
+    a:"apple",
+    b:"ball",
+    c:"cat"
+}
+console.log(dict.b)
