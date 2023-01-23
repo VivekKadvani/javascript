41995612bdf763df4dcabb33c65260f4a3204685
Vivek Kadvani <vivek.k@simformsolutions.com>
2023-01-23 16:55:21 +0530

    operator.js file added

---
+++ operator.js
@@ -0,0 +1,50 @@
+//arithmatic operator
+let a=78
+let b=4
+
+console.log("Erithmetic Operator :  ")
+console.log("A + B = ",a+b)
+console.log("A - B = ",a-b)
+console.log("A * B = ",a*b)
+console.log("A / B = ",a/b)
+console.log("A ** B = ",a**b)
+console.log("A % B = ",a%b)
+console.log("A++ = ",a++ , "++A =",++a)
+console.log("B-- = ",b-- , "--B =",--b)
+
+//assignment operator
+
+let c=10
+let d=15
+console.log("Assignment Operator :  ")
+console.log("c+=2 ",c+=2)
+console.log("c-=2 ",c-=2)
+console.log("c*=3 ",c*=3)
+console.log("c/=2 ",c/=2)
+console.log("c%=2 ",c%=2)
+console.log("d**=2 ",d**=2)
+
+
+
+console.log("Comparison Operator :  ")
+console.log("2==2",2==2)
+console.log("2!=2",2!=2)
+console.log("2===2",2===2)
+console.log("2!==2",2!==2)
+console.log("4<2",4<2)
+console.log("4>2",4>2)
+console.log("2<=2",2<=2)
+console.log("2>=2",2>=2)
+
+
+console.log("logical operator")
+console.log("true && true = ",true && true)
+console.log("true && false = ",true && false)
+console.log("false && true = ",false && true)
+console.log("false && false = ",false && false)
+console.log("true || true = ",true || true)
+console.log("true || false = ",true || false)
+console.log("false || true = ",false || true)
+console.log("false || false = ",false || false)
+console.log("!false ",!false)
+console.log("!true ",!true)
