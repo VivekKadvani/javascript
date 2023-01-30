cca16564f27c6a6de7a975899b1d18c5137531c1
Vivek Kadvani <vivek.k@simformsolutions.com>
2023-01-30 13:51:08 +0530

    learn about class

---
+++ class.js
@@ -0,0 +1,13 @@
+class car{
+    constructor(namer,prise){
+        this.name=namer,
+        this.price=prise
+    }
+}
+class seller extends car {
+    seller="vivek"
+}
+
+// const obj = new car("mustangGT",10000000);
+const obj =new seller("mustang",12234)
+console.log(obj.name)
